c9ed8315f666a09438b82f4c615566ceb3bd46c8
ViAarisKea <viaa0001@stud.kea.dk>
2024-02-21 11:05:38 +0100

    implement reoccuring menu (user story #2: after having created a movie the choice appears again)

--- src/main/java/filmsamling/Main.java
+++ src/main/java/filmsamling/Main.java
@@ -7,46 +7,41 @@ public class Main {
     private static MovieCollection movieCollection = new MovieCollection();
 
     public static void main(String[] args) {
-
-
-        showMenu();
         scanAndCreateMovie();
 
     }
 
-    public static void showMenu(){
+    public static void showMenu() {
         System.out.println("Welcome to my movie collection!\n 1. Add a movie\n 2. Exit");
     }
 
-    public static void scanAndCreateMovie(){
+    public static void scanAndCreateMovie() {
         Scanner scanner = new Scanner(System.in);
 
-        while(true){
-
-
-            System.out.println("Title of the movie?");
-            String title = scanner.nextLine();
-            System.out.println("Movie director?");
-            String director = scanner.nextLine();
-            System.out.println("Year of creation?");
-            int year = scanner.nextInt();
-            System.out.println("Is it coloured? Answer true if it's colored and false if it's black&white");
-            boolean isColoured = scanner.nextBoolean();
-            System.out.println("Length in minutes?");
-            int length = scanner.nextInt();
-            scanner.nextLine();
-            System.out.println("Genre?");
-            String genre = scanner.nextLine();
-
-
-
-            System.out.println("Adding new movie to the collection...");
-            movieCollection.addMovie(title, director, year, isColoured, length, genre);
-
-            System.out.println("Press q to quit or enter to continue");
-
+        while (true) {
+            showMenu();
             String key = scanner.nextLine();
-            if(key.equals("q")) break;
+            if (key.equals("2")) {
+                break;
+            } else if (key.equals("1")) {
+                System.out.println("Title of the movie?");
+                String title = scanner.nextLine();
+                System.out.println("Movie director?");
+                String director = scanner.nextLine();
+                System.out.println("Year of creation?");
+                int year = scanner.nextInt();
+                System.out.println("Is it coloured? Answer true if it's colored and false if it's black&white");
+                boolean isColoured = scanner.nextBoolean();
+                System.out.println("Length in minutes?");
+                int length = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("Genre?");
+                String genre = scanner.nextLine();
+
+
+                System.out.println("Adding new movie to the collection...");
+                movieCollection.addMovie(title, director, year, isColoured, length, genre);
+            }
 
         }
     }
