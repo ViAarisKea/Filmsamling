a1e3fe26fed88dcdb487bc0ceefa213faf524116
ViAarisKea <viaa0001@stud.kea.dk>
2024-02-22 15:01:47 +0100

    change scanAndAddMovie method - now it has more functionality. user story #4: user can see all movies

--- src/main/java/filmsamling/Main.java
+++ src/main/java/filmsamling/Main.java
@@ -7,22 +7,24 @@ public class Main {
     private static Controller controller = new Controller();
 
     public static void main(String[] args) {
-        scanAndCreateMovie();
-
+        processUserActions();
     }
 
     public static void showMenu() {
-        System.out.println("Welcome to my movie collection!\n 1. Add a movie\n 2. Exit");
+        System.out.println("Welcome to my movie collection!\n" +
+                " 1. Add a movie\n" +
+                " 2. View the whole movie collection\n" +
+                " 3. Exit");
     }
 
-    public static void scanAndCreateMovie() {
+    public static void processUserActions() {
         Scanner scanner = new Scanner(System.in);
 
 
         while (true) {
             showMenu();
             String key = scanner.nextLine();
-            if (key.equals("2")) {
+            if (key.equals("3")) {
                 break;
             } else if (key.equals("1")) {
                 System.out.println("Title of the movie?");
@@ -42,13 +44,15 @@ public class Main {
 
                 System.out.println("Adding new movie to the collection...");
                 controller.addMovieToMovieCollection(title, director, year, isColoured, length, genre);
+            } else if (key.equals("2")) {
+                printAllMovies();
             }
 
         }
     }
 
-    public static void printAllMovies(){
-        for(Movie movie : controller.findAllMovieFromMovieCollection()){
+    public static void printAllMovies() {
+        for (Movie movie : controller.findAllMovieFromMovieCollection()) {
             System.out.println(movie.toString());
         }
     }
